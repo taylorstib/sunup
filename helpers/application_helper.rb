280534a22caee36b6b4f2b3169fb7d4fabe8cb91
Taylor Stib <taylor@raddevelopment.io>
2015-07-07 14:24:38 -0700

    Put helpers in a module

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -1,17 +1,17 @@
-def format_thousands number
-  number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-end
+module ApplicationHelper
 
-def day_difference_to_words diff
-  if diff > 7 && diff.remainder(7) == 1
-    "#{diff / 7} weeks and #{diff.remainder(7)} day"
-  elsif diff > 7
-    "#{diff / 7} weeks and #{diff.remainder(7)} days"
-  else
-    ' '
+  def format_thousands number
+    number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   end
-end
 
-def future_or_past
-  
+  def day_difference_to_words diff
+    if diff > 7 && diff.remainder(7) == 1
+      "#{diff / 7} weeks and #{diff.remainder(7)} day"
+    elsif diff > 7
+      "#{diff / 7} weeks and #{diff.remainder(7)} days"
+    else
+      ' '
+    end
+  end
+
 end
